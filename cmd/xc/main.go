a809ab169b2322fa58869644d3ab7b6631135070
joe-davidson1802 <joe.davidson.21111@gmail.com>
2022-02-04 15:15:30 +0000

    feat: refactor cmd

--- cmd/xc/main.go
+++ cmd/xc/main.go
@@ -9,6 +9,7 @@ import (
 	"runtime/debug"
 	"strings"
 
+	"github.com/joe-davidson1802/xc/models"
 	"github.com/joe-davidson1802/xc/parser"
 	"github.com/posener/complete"
 )
@@ -17,147 +18,124 @@ var (
 	version = ""
 )
 
-func completion(fileName string) bool {
-	cmp := complete.New("xc", complete.Command{
-		GlobalFlags: complete.Flags{
-			"-version": complete.PredictNothing,
-			"-h":       complete.PredictNothing,
-			"-short":   complete.PredictNothing,
-			"-help":    complete.PredictNothing,
-			"-f":       complete.PredictFiles("*.md"),
-			"-file":    complete.PredictFiles("*.md"),
-		},
-	})
-	b, err := os.Open(fileName)
-	if err == nil {
-		p, err := parser.NewParser(b)
-		if err != nil {
-			return false
-		}
-		t, err := p.Parse()
-		if err != nil {
-			return false
-		}
-		s := make(map[string]complete.Command)
-		for _, ta := range t {
-			s[ta.Name] = complete.Command{}
-		}
-		cmp.Command.Sub = s
-	}
-	cmp.CLI.InstallName = "complete"
-	cmp.CLI.UninstallName = "uncomplete"
-	cmp.AddFlags(nil)
-
-	flag.Parse()
-
-	return cmp.Complete()
+type config struct {
+	version, help, short, md bool
+	filename                 string
 }
 
-func main() {
+var cfg = config{}
+
+func flags() {
 	log.SetFlags(0)
 	log.SetOutput(os.Stderr)
-
 	flag.Usage = func() {
 		fmt.Println("xc - list tasks")
 		fmt.Println("xc [task...] - run tasks")
 		flag.PrintDefaults()
 	}
+	flag.BoolVar(&cfg.version, "version", false, "show xc version")
+	flag.BoolVar(&cfg.help, "help", false, "shows xc usage")
+	flag.BoolVar(&cfg.help, "h", false, "shows xc usage")
+	flag.StringVar(&cfg.filename, "file", "README.md", "specify markdown file that contains tasks")
+	flag.StringVar(&cfg.filename, "f", "README.md", "specify markdown file that contains tasks")
+	flag.BoolVar(&cfg.short, "short", false, "list task names in a short format")
+	flag.BoolVar(&cfg.short, "s", false, "list task names in a short format")
+	flag.BoolVar(&cfg.md, "md", false, "print the markdown for a task rather than running it")
+	flag.Parse()
+}
 
-	var (
-		versionFlag bool
-		helpFlag    bool
-		fileName    string
-		short       bool
-		md          bool
-	)
-
-	flag.BoolVar(&versionFlag, "version", false, "show xc version")
-	flag.BoolVar(&helpFlag, "help", false, "shows xc usage")
-	flag.BoolVar(&helpFlag, "h", false, "shows xc usage")
-	flag.StringVar(&fileName, "file", "README.md", "specify markdown file that contains tasks")
-	flag.StringVar(&fileName, "f", "README.md", "specify markdown file that contains tasks")
-	flag.BoolVar(&short, "short", false, "list task names in a short format")
-	flag.BoolVar(&md, "md", false, "print the markdown for a task rather than running it")
-
-	if completion(fileName) {
-		return
-	}
-
-	b, err := os.Open(fileName)
+func parse() (t models.Tasks, err error) {
+	b, err := os.Open(cfg.filename)
 	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+		return
 	}
 	p, err := parser.NewParser(b)
 	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+		return
 	}
-	t, err := p.Parse()
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+	t, err = p.Parse()
+	return
+}
+
+func printTasks(t models.Tasks) {
+	print := printTask
+	if cfg.short {
+		print = func(t models.Task, maxLen int) { fmt.Println(t.Name) }
 	}
-	if versionFlag {
+	maxLen := 0
+	for _, n := range t {
+		if len(n.Name) > maxLen {
+			maxLen = len(n.Name)
+		}
+	}
+	for _, n := range t {
+		print(n, maxLen)
+	}
+	return
+}
+
+func printTask(t models.Task, maxLen int) {
+	padLen := maxLen - len(t.Name)
+	pad := strings.Repeat(" ", padLen)
+	var desc []string
+	for _, d := range t.Description {
+		desc = append(desc, fmt.Sprintf("%s", d))
+	}
+	if len(t.DependsOn) > 0 {
+		desc = append(desc, fmt.Sprintf("Requires:  %s", strings.Join(t.DependsOn, ", ")))
+	}
+	if len(desc) == 0 {
+		desc = append(desc, t.Commands...)
+	}
+	fmt.Printf("    %s%s  %s\n", t.Name, pad, desc[0])
+	for _, d := range desc[1:] {
+		fmt.Printf("    %s  %s\n", strings.Repeat(" ", maxLen), d)
+	}
+}
+
+func main() {
+	flags()
+	t, err := parse()
+	if completion(t) {
+		return
+	}
+	// xc -version
+	if cfg.version {
 		fmt.Printf("xc version: %s\n", getVersion())
 		return
 	}
-
-	if helpFlag {
+	// xc -h / xc -help
+	if cfg.help {
 		flag.Usage()
 		return
 	}
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	tav := getArgs()
-	if len(tav) == 0 && !short {
-		fmt.Println("tasks:")
-		maxLen := 0
-		for _, n := range t {
-			if len(n.Name) > maxLen {
-				maxLen = len(n.Name)
-			}
-		}
-		for _, n := range t {
-			padLen := maxLen - len(n.Name)
-			pad := strings.Repeat(" ", padLen)
-			var desc []string
-			if n.ParsingError != "" {
-				desc = append(desc, fmt.Sprintf("Parsing Error: %s", n.ParsingError))
-			}
-			for _, d := range n.Description {
-				desc = append(desc, fmt.Sprintf("%s", d))
-			}
-			if len(n.DependsOn) > 0 {
-				desc = append(desc, fmt.Sprintf("Requires:  %s", strings.Join(n.DependsOn, ", ")))
-			}
-			if len(desc) == 0 {
-				desc = append(desc, n.Commands...)
-			}
-			fmt.Printf("    %s%s  %s\n", n.Name, pad, desc[0])
-			for _, d := range desc[1:] {
-				fmt.Printf("    %s  %s\n", strings.Repeat(" ", maxLen), d)
-			}
-		}
+	// xc
+	if len(tav) == 0 {
+		printTasks(t)
 		return
 	}
-	if len(tav) == 0 && short {
-		for _, n := range t {
-			fmt.Println(n.Name)
-		}
-		return
-	}
-	if md {
-		if len(tav) != 1 {
-			fmt.Printf("md requires 1 task, got: %d\n", len(tav))
+	// xc -md task1 task2
+	if cfg.md {
+		if len(tav) == 0 {
+			fmt.Println("md requires atleast 1 task")
 			os.Exit(1)
 		}
-		ta, ok := t.Get(tav[0])
-		if !ok {
-			fmt.Printf("%s is not a task\n", tav[0])
+		for _, tv := range tav {
+			ta, ok := t.Get(tv)
+			if !ok {
+				fmt.Printf("%s is not a task\n", tav[0])
+			}
+			ta.Display(os.Stdout)
 		}
-		ta.Display(os.Stdout)
 		return
 
 	}
+	// xc task1 task2
 	for _, tav := range tav {
 		err = t.ValidateDependencies(tav, []string{})
 		if err != nil {
@@ -195,4 +173,26 @@ func getVersion() string {
 	}
 
 	return version
+}
+
+func completion(t models.Tasks) bool {
+	cmp := complete.New("xc", complete.Command{
+		GlobalFlags: complete.Flags{
+			"-version": complete.PredictNothing,
+			"-h":       complete.PredictNothing,
+			"-short":   complete.PredictNothing,
+			"-help":    complete.PredictNothing,
+			"-f":       complete.PredictFiles("*.md"),
+			"-file":    complete.PredictFiles("*.md"),
+		},
+	})
+	s := make(map[string]complete.Command)
+	for _, ta := range t {
+		s[ta.Name] = complete.Command{}
+	}
+	cmp.Command.Sub = s
+	cmp.CLI.InstallName = "complete"
+	cmp.CLI.UninstallName = "uncomplete"
+	cmp.AddFlags(nil)
+	return cmp.Complete()
 }
