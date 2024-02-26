9d43268072ba211973e67f82e5ac2e7c3c1a96af
Adrian Hesketh <adrianhesketh@hushmail.com>
2024-02-26 12:45:49 +0000

    chore: update nix flake to remove deprecation warning

--- xc.nix
+++ xc.nix
@@ -3,12 +3,12 @@
 pkgs.buildGoModule rec {
   pname = "xc";
   version = "v0.8.0";
-  subPackages = ["cmd/xc"];
+  subPackages = [ "cmd/xc" ];
   src = pkgs.fetchFromGitHub {
     owner = "joerdav";
     repo = "xc";
     rev = version;
     sha256 = "vTyCS85xbJnAgbasWD6LFxij9EezzlJ1pyvCJptqmOU=";
   };
-  vendorSha256 = "EbIuktQ2rExa2DawyCamTrKRC1yXXMleRB8/pcKFY5c=";
+  vendorHash = "sha256-EbIuktQ2rExa2DawyCamTrKRC1yXXMleRB8/pcKFY5c=";
 }
