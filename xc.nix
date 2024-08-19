48e28d6f29623b0c2eedce688fcb7d29f0d2976e
joerdav <joe.davidson.21111@gmail.com>
2024-08-19 15:25:53 +0000

    [Automated]: Update Nix Flake

--- xc.nix
+++ xc.nix
@@ -2,13 +2,13 @@
 
 pkgs.buildGoModule rec {
   pname = "xc";
-  version = "v0.8.4";
+  version = "v1";
   subPackages = [ "cmd/xc" ];
   src = pkgs.fetchFromGitHub {
     owner = "joerdav";
     repo = "xc";
     rev = version;
-    sha256 = "AttYWpqD8m+CaIaX7WVNlG507uVykIVgArLebRNEUpM=";
+    sha256 = "p6rXzNiDm2uBvO1MLzC5pJp/0zRNzj/snBzZI0ce62s=";
   };
   vendorHash = "sha256-EbIuktQ2rExa2DawyCamTrKRC1yXXMleRB8/pcKFY5c=";
 }
