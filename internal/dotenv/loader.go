7793b8205865607a12308f1dd0adcdbae3d47b4a
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2026-04-05 14:26:36 -0400

    add godotenv dependency

---
+++ internal/dotenv/loader.go
@@ -0,0 +1,11 @@
+package dotenv
+
+import (
+	_ "github.com/joho/godotenv" // Will be used in implementation
+)
+
+// Load loads .env files from the specified directory.
+// This is a placeholder that will be implemented via TDD.
+func Load(dir string) error {
+	return nil
+}
